bc0dc33ff272491e6b66debc32b5f6dcf4eee6e4
barry gardiner <G00398792@gmit.ie>
2021-02-08 21:39:38 +0000

    updating

--- bmi.py
+++ bmi.py
@@ -2,9 +2,16 @@
 # This program calculates your Body Mass Index (BMI).
 # author: Barry Gardiner
 
-height = int(input("Please Enter Your Height In Centimetres:"))
-weight = int(input("Please Enter Your Weight in kilograms:"))
+#User is prompted to enter height and weight as a float 
+# (real number i.e. 1.0) number. The users weight is divided
+# by the height in metres to the power of 2. The output
+# is printed to the screen. The code "{:.2f}'.format(BMI)" 
+# specifies the format of the string. "2f" represents a floating
+# point number to a precision of 2 decimal places. 
+
+height = float(input("Please Enter Your Height In Centimetres:"))
+weight = float(input("Please Enter Your Weight in kilograms:"))
 
 bmi = ((weight) / ((height/100))**2)#Formula: weight (kg) / [height (m)]2
 
-print('Your BMI is {}'.format(round(bmi,2)))#Using format to fromat BMI
+print('BMI is {:.2f}.'.format(bmi))
